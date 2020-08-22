055525a6208f776809ac705efef8970377a08d51
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2020-08-22 10:31:56 +0530

    banner added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React,{useEffect} from "react";
 import "./styles/App.scss";
 import Header from "./components/header";
+import Banner from "./components/banner";
 
 function App() {
+    useEffect(()=>{
+        let vh = window.innerHeight *.01;
+        document.documentElement.style.setProperty('--vh',`${vh}px`)
+
+    },[])
   return (
     <div className='App'>
      <Header/>
+     <Banner/>
     </div>
   );
 }
