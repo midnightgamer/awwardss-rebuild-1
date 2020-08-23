a618fbd15eb1aa57a1ed8027dcfbaf313b1613eb
Pankaj Jaiswal <jaiswal1999.pj@gmail.com>
2020-08-23 19:29:41 +0530

    rouet added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import React, { useEffect } from "react";
 import "./styles/App.scss";
 import gsap from "gsap";
+import { Route } from "react-router-dom";
 import Header from "./components/header";
+// Components
+import About from "./pages/about";
+import Approch from "./pages/approach";
+import Services from "./pages/services";
+import CaseStudies from "./pages/caseStudies";
 import Home from "./pages/home";
 
+// Routes
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    Component: Home,
+  },
+  {
+    path: "/case-studies",
+    name: "Case Studies",
+    Component: CaseStudies,
+  },
+  {
+    path: "/approach",
+    name: "Approach",
+    Component: Approch,
+  },
+  {
+    path: "/services",
+    name: "Services",
+    Component: Services,
+  },
+  {
+    path: "/about",
+    name: "About us",
+    Component: About,
+  },
+];
 function App() {
   useEffect(() => {
     const vh = window.innerHeight * 0.01;
@@ -11,10 +45,14 @@ function App() {
     gsap.to("body", { css: { visibility: "visible" } });
   });
   return (
-    <div className="App">
+    <>
       <Header />
-      <Home />
-    </div>
+      <div className="App">
+        {routes.map(({ path, Component }) => (
+          <Route key={path} exact component={Component} path={path} />
+        ))}
+      </div>
+    </>
   );
 }
 
